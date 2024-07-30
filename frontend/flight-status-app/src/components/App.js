fd37a0d0f97419e60d732ac856c61a8e0cc82d95
Kalp Jain <98029102+Kalpbiz@users.noreply.github.com>
2024-07-30 11:14:12 -0700

    Update App.js

--- frontend/flight-status-app/src/components/App.js
+++ frontend/flight-status-app/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Login from './components/Login';
 import FlightStatus from './components/FlightStatus';
+import './App.css';
 
 const App = () => {
   const [token, setToken] = useState(null);
