6cc78fa1baab167a609635be71382136e6da5e09
Kalp Jain <98029102+Kalpbiz@users.noreply.github.com>
2024-07-30 10:54:05 -0700

    Create App.js

---
+++ frontend/flight-status-app/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Login from './components/Login';
+import FlightStatus from './components/FlightStatus';
+
+const App = () => {
+  const [token, setToken] = useState(null);
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
+  return <FlightStatus token={token} />;
+};
+
+export default App;
